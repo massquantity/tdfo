4014bfebea7c0212c279c068fae137bbd88b59a8
massquantity <wdmjjxg@163.com>
2023-08-26 07:52:19 +0800

    [torchrec] Use polars for fast data preprocessing

--- torchrec/preprocessing.py
+++ torchrec/preprocessing.py
@@ -1,197 +1,315 @@
-import random
-from collections import Counter
-from typing import Any, Dict, List
+import json
+import math
+import time
+from pathlib import Path
+from typing import List, Tuple
 
 import numpy as np
-import pandas as pd
+import polars as pl
 
-from utils import Config
+from utils import read_configs
+
+MIN_INTERACTIONS = 20
+MAX_INTERACTIONS = 200
+PAD_ID = 0
+MASK_ID = -1  # set when n_items is available
+EVAL_NEG_NUM = 100  # follow the paper setting
+FILE_NUM = 1
+
+DTYPES = {
+    "user_id": pl.Int32,
+    "book_id": pl.Int32,
+    "is_read": pl.Int8,
+    "is_reviewed": pl.Int8,
+    "rating": pl.Int8,
+}
 
 
-class Preprocessor:
-    def __init__(self, config: Config):
-        self.raw_data = pd.read_csv(
-            config.data_dir / config.train_data,
-            sep="::",
-            names=["uid", "sid", "rating", "timestamp"],
+def read_original_data(data_dir: Path) -> pl.DataFrame:
+    """Read and transform data based on the following steps:
+
+    1. Only keep users with MIN_INTERACTIONS to MAX_INTERACTIONS interactions.
+    2. Sort `book_id` for every user for further data splitting.
+    """
+    data = pl.scan_csv(data_dir / "goodreads_interactions.csv", dtypes=DTYPES)
+    data = data.filter(
+        (
+            (pl.col("book_id").count().over("user_id") >= MIN_INTERACTIONS)
+            & (pl.col("book_id").count().over("user_id") <= MAX_INTERACTIONS)
+        ).alias("num_interactions")
+    )
+    item_sort_expr = pl.col("book_id").sort().over("user_id")
+    data = data.select("user_id", item_sort_expr).collect(streaming=True)
+    return data
+
+
+def map_ids(df: pl.DataFrame) -> Tuple[pl.DataFrame, int, int]:
+    def _get_sparse_mapping(col_name: str) -> dict:
+        unique_vals = sorted(df.select(pl.col(col_name).unique()).to_series().to_list())
+        mapping = dict(zip(unique_vals, range(1, len(unique_vals) + 1)))
+        return mapping
+
+    user_id_mapping = _get_sparse_mapping("user_id")
+    item_id_mapping = _get_sparse_mapping("book_id")
+    data = df.select(
+        pl.col("user_id").map_dict(
+            user_id_mapping, default=PAD_ID, return_dtype=pl.Int32
+        ),
+        pl.col("book_id").map_dict(
+            item_id_mapping, default=PAD_ID, return_dtype=pl.Int32
+        ),
+    )
+
+    n_users = len(user_id_mapping)
+    n_items = len(item_id_mapping)
+    global MASK_ID
+    MASK_ID = n_items + 1  # item id range: [1, n_items]
+
+    assert data.get_column("user_id").min() == 1
+    assert data.get_column("user_id").max() == n_users
+    assert data.get_column("book_id").min() == 1
+    assert data.get_column("book_id").max() == n_items
+    return data, n_users, n_items
+
+
+def get_item_popularity(df: pl.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
+    popularity = df.groupby("book_id").count().sort("count", descending=True)
+    items = popularity.get_column("book_id").to_numpy()
+    counts = popularity.get_column("count").to_numpy()
+    item_probs = counts / np.sum(counts)
+    return items, item_probs
+
+
+def split_data(df: pl.DataFrame) -> pl.DataFrame:
+    """Split interaction items for each user.
+
+    eval and test data will keep the last two items, the rest items are used for training.
+    """
+
+    def _split_train_eval_test(interaction: pl.Series) -> pl.Series:
+        interaction = interaction.sort()
+        length = len(interaction)
+        train_items = interaction.head(length - 2)
+        eval_items = interaction.take(length - 2)
+        test_items = interaction.take(length - 1)
+        return train_items, eval_items, test_items
+
+    data = (
+        df.lazy()
+        .groupby("user_id")
+        .agg(
+            pl.col("book_id").apply(_split_train_eval_test).alias("total_interactions")
         )
-        self.max_len = config.max_len
-        self.sliding_step = config.sliding_step
-        self.mask_prob = config.mask_prob
-        self.user_count = None
-        self.item_count = None
-        self.mask_token = None
-        self.pad_token = 0
-        self.np_rng = np.random.default_rng(config.seed)
+    )
+    return data.select(
+        "user_id",
+        pl.col("total_interactions").list.get(0).alias("train_interactions"),
+        pl.col("total_interactions").list.get(1).alias("eval_interactions"),
+    ).collect()
 
-    def get_processed_dataframes(self) -> Dict[str, Any]:
-        df = self._preprocess()
-        return df
 
-    def _preprocess(self) -> Dict[str, Any]:
-        df, umap, smap = self._densify_index()
-        train, val, test = self._split_df(df, len(umap))
-        df = {"train": train, "val": val, "test": test, "umap": umap, "smap": smap}
-        self.user_count = len(df["umap"])
-        self.item_count = len(df["smap"])
-        self.mask_token = self.item_count + 1  # item id range: [1, self.item_count]
-        final_df = self._mask_and_labels(self._generate_negative_samples(df))
-        return final_df
+def gen_train_mask_data(
+    df: pl.DataFrame, n_items: int, mask_prob: float, rng: np.random.Generator
+) -> pl.DataFrame:
+    """Bert-like data masking.
 
-    def _densify_index(self):
-        df = self.raw_data
-        umap = {u: i for i, u in enumerate(set(df["uid"]))}
-        smap = {s: i + 1 for i, s in enumerate(set(df["sid"]))}
-        df["uid"] = df["uid"].map(umap)
-        df["sid"] = df["sid"].map(smap)
-        return df, umap, smap
+    Items with probability of `mask_prob` are masked as MASK_ID
+    and probability of `mask_prob * 0.2` are masked as random items.
 
-    @staticmethod
-    def _split_df(df: pd.DataFrame, user_count: int):
-        # leave-one-out in the paper
-        print("Splitting")
-        user_group = df.groupby("uid")
-        user2items = user_group.apply(
-            lambda d: list(d.sort_values(by="timestamp")["sid"])
+    Labels are the original items if masking else PAD_ID,
+    and grads of PAD_ID will be ignored in `nn.CrossEntropyLoss`
+    """
+    random_item_prob = mask_prob * 0.2
+    data = df.select("user_id", "train_interactions").explode("train_interactions")
+    data = data.lazy().with_columns(
+        mask_prob_col=pl.lit(rng.random(size=len(data), dtype=np.float32)),
+        random_items_col=pl.lit(
+            rng.integers(1, n_items + 1, size=len(data), dtype=np.int32)
+        ),
+    )
+    masked_items_col = (
+        pl.when(pl.col("mask_prob_col") < random_item_prob)
+        .then(pl.col("random_items_col"))
+        .when(pl.col("mask_prob_col") <= mask_prob)
+        .then(pl.lit(MASK_ID, dtype=pl.Int32))
+        .otherwise(pl.col("train_interactions"))
+        .alias("train_interactions")
+    )
+    label_col = (
+        pl.when(pl.col("mask_prob_col") <= mask_prob)
+        .then(pl.col("train_interactions"))
+        .otherwise(pl.lit(PAD_ID, dtype=pl.Int32))
+        .alias("labels")
+    )
+    return data.select("user_id", masked_items_col, label_col).collect()
+
+
+def get_masked_ratio(train_data: pl.DataFrame) -> float:
+    masked_length = len(train_data.filter(pl.col("train_interactions") == MASK_ID))
+    return masked_length / len(train_data["user_id"])
+
+
+def gen_sliding_seq_slow(
+    df: pl.DataFrame, seq_len: int, sliding_step: int
+) -> pl.DataFrame:
+    """This function is slow in polars due to the python runtime."""
+
+    def _sliding_window(seq: pl.Series) -> List[pl.List]:
+        length = len(seq)
+        # total = pl.concat([seq, pl.Series("mask", [PAD_ID] * seq_len, dtype=x.dtype)])
+        total = seq.extend_constant(PAD_ID, n=seq_len)
+        return [total.slice(i, seq_len) for i in range(0, length, sliding_step)]
+
+    data = (
+        df.lazy()
+        .groupby("user_id")
+        .agg(
+            pl.col("train_interactions").apply(_sliding_window),
+            pl.col("labels").apply(_sliding_window),
         )
-        train, val, test = {}, {}, {}
-        for user in range(user_count):
-            items = user2items[user]
-            train[user], val[user], test[user] = items[:-2], items[-2:-1], items[-1:]
-        return train, val, test
+    )
+    # explode nested list to list
+    return data.explode(["train_interactions", "labels"]).collect()
 
-    def _generate_negative_samples(
-        self,
-        df: Dict[str, Any],
-    ) -> Dict[str, Any]:
-        # follow the paper, no negative samples in training set
-        # 100 negative samples in test set, 2 for random to save time
-        test_set_sample_size = 100
 
-        # use popularity random sampling align with paper
-        popularity = Counter()
-        for user in range(self.user_count):
-            popularity.update(df["train"][user])
-            popularity.update(df["val"][user])
-            popularity.update(df["test"][user])
-        items_list, freq = zip(*popularity.items())
-        freq_sum = sum(freq)
-        prob = [float(i) / freq_sum for i in freq]
-        test_negative_samples = {}
-        min_size = test_set_sample_size
-        print("Sampling negative items")
-        for user in range(self.user_count):
-            seen = set(df["train"][user])
-            seen.update(df["val"][user])
-            seen.update(df["test"][user])
-            samples = []
-            while len(samples) < test_set_sample_size:
-                sampled_ids = self.np_rng.choice(
-                    items_list, test_set_sample_size * 2, replace=False, p=prob
-                )
-                sampled_ids = [x for x in sampled_ids if x not in seen]
-                samples.extend(sampled_ids[:])
-            min_size = min_size if min_size < len(samples) else len(samples)
-            test_negative_samples[user] = samples
-        if min_size == 0:
-            raise RuntimeError(
-                "we sampled 0 negative samples for a user, please increase the data size"
-            )
-        test_negative_samples = {
-            key: value[:min_size] for key, value in test_negative_samples.items()
-        }
-        df["test_negative_samples"] = test_negative_samples
-        return df
+def gen_sliding_seq(df: pl.DataFrame, seq_len: int, sliding_step: int) -> pl.DataFrame:
+    user_groupby = df.groupby("user_id").agg("train_interactions", "labels")
+    user_ids = user_groupby.get_column("user_id").to_numpy().tolist()
+    item_ids = user_groupby.get_column("train_interactions").to_numpy().tolist()
+    labels = user_groupby.get_column("labels").to_numpy().tolist()
+    total_users, total_items, total_labels = [], [], []
+    for user, seqs, lbs in zip(user_ids, item_ids, labels):
+        pad_len = _get_pad_size(seqs, seq_len, sliding_step)
+        padded_seqs = np.append(seqs, ([0] * pad_len))
+        padded_labels = np.append(lbs, ([0] * pad_len))
+        for i in range(0, len(seqs), sliding_step):
+            total_users.append(user)
+            total_items.append(padded_seqs[i : i + seq_len])
+            total_labels.append(padded_labels[i : i + seq_len])
 
-    def _generate_masked_train_set(self, train_data: Dict[int, List[int]]) -> pd.DataFrame:
-        df = []
-        for user, seqs in train_data.items():
-            # sliding_step = int(0.1 * self.max_len)
-            # beg_idx = list(
-            #    range(
-            #        len(seq) - self.max_len,
-            #        0,
-            #        -self.sliding_step,
-            #    )
-            # )
-            # beg_idx.append(0)
-            # seqs = [seq[i : i + self.max_len] for i in beg_idx[::-1]]
-            for i in range(0, len(seqs), self.sliding_step):
-                seq = seqs[i: i + self.max_len]
-                tokens = []
-                labels = []
-                for s in seq:
-                    prob = random.random()
-                    if prob < self.mask_prob:
-                        prob /= self.mask_prob
+    seq_data = pl.DataFrame(
+        {
+            "user_id": total_users,
+            "train_interactions": total_items,
+            "labels": total_labels,
+        },
+        schema={
+            "user_id": pl.Int32,
+            "train_interactions": pl.List(pl.Int32),
+            "labels": pl.List(pl.Int32),
+        },
+    )
+    return seq_data
 
-                        if prob < 0.8:
-                            tokens.append(self.mask_token)
-                        else:
-                            tokens.append(random.randint(1, self.item_count))
-                        labels.append(s)
-                    else:
-                        tokens.append(s)
-                        labels.append(self.pad_token)  # grads will be ignored in `nn.CrossEntropyLoss`
-                if len(tokens) < self.max_len:
-                    mask_len = self.max_len - len(tokens)
-                    tokens = [self.pad_token] * mask_len + tokens
-                    labels = [self.pad_token] * mask_len + labels
-                df.append([user, tokens, labels])
-        return pd.DataFrame(df, columns=["user", "seqs", "labels"])
 
-    def _generate_labeled_eval_set(
-        self,
-        train_data: Dict[int, List[int]],
-        eval_data: Dict[int, List[int]],
-        negative_samples: Dict[int, List[int]],
-    ) -> pd.DataFrame:
-        df = []
-        for user, seqs in train_data.items():
-            answer = eval_data[user]
-            negs = negative_samples[user]
-            candidates = answer + negs
-            labels = [1] * len(answer) + [0] * len(negs)
-            tokens = seqs
-            tokens = tokens + [self.mask_token]
-            tokens = tokens[-self.max_len :]
-            if len(tokens) < self.max_len:
-                padding_len = self.max_len - len(tokens)
-                tokens = [self.pad_token] * padding_len + tokens
-            df.append([user, tokens, candidates, labels])
-        return pd.DataFrame(df, columns=["user", "seqs", "candidates", "labels"])
+def _get_pad_size(seqs: List[int], seq_len: int, sliding_step: int) -> int:
+    border_index = (len(seqs) - 1) // sliding_step * sliding_step
+    return border_index + seq_len - len(seqs)
 
-    def _mask_and_labels(self, df: Dict[str, Any]) -> Dict[str, Any]:
-        masked_train_set = self._generate_masked_train_set(df["train"])
-        labled_val_set = self._generate_labeled_eval_set(
-            df["train"],
-            df["val"],
-            df["test_negative_samples"],
-        )
-        train_with_valid = {
-            key: df["train"].get(key, []) + df["val"].get(key, [])
-            for key in set(list(df["train"].keys()) + list(df["val"].keys()))
-        }
-        labled_test_set = self._generate_labeled_eval_set(
-            train_with_valid, df["test"], df["test_negative_samples"]
-        )
-        masked_df = {
-            "train": masked_train_set,
-            "val": labled_val_set,
-            "test": labled_test_set,
-            "umap": df["umap"],
-            "smap": df["smap"],
-        }
-        return masked_df
+
+def get_eval_seqs(df: pl.LazyFrame, seq_len: int, data_size: int) -> pl.LazyFrame:
+    """Get interactions for evaluation from last `seq_len` train_interactions, padded with PAD_ID"""
+    pad_items = np.full([data_size, seq_len], fill_value=PAD_ID, dtype=np.int32)
+    data = df.with_columns(
+        pad_items=pl.lit(pad_items, dtype=pl.Int32),
+        mask_items=pl.lit(MASK_ID, dtype=pl.Int32),
+    )
+    eval_seqs = pl.concat_list(["pad_items", "train_interactions", "mask_items"])
+    return data.select(
+        "user_id", eval_seqs.list.tail(seq_len).alias("eval_seqs"), "eval_interactions"
+    )
+
+
+def sample_negatives(
+    data_size: int,
+    rng: np.random.Generator,
+    items: np.ndarray,
+    probs: np.ndarray,
+) -> np.ndarray:
+    """Sample `EVAL_NEG_NUM` negatives for each user."""
+    total_size = data_size * EVAL_NEG_NUM
+    # avoid exceeding total item num in `rng.choice`
+    step = len(items) // 5 // EVAL_NEG_NUM
+    negative_samples = []
+    for i in range(0, data_size, step):
+        negs = rng.choice(items, step * EVAL_NEG_NUM, replace=False, p=probs)
+        negative_samples.extend(negs.tolist())
+    negative_samples = np.array(negative_samples[:total_size], dtype=np.int32)
+    return negative_samples.reshape(data_size, EVAL_NEG_NUM)
+
+
+def gen_eval_data(
+    df: pl.DataFrame,
+    seq_len: int,
+    rng: np.random.Generator,
+    items: np.ndarray,
+    probs: np.ndarray,
+) -> pl.DataFrame:
+    data = get_eval_seqs(df.lazy(), seq_len, len(df))
+    negative_samples = sample_negatives(len(df), rng, items, probs)
+    data = data.with_columns(negative_samples=pl.lit(negative_samples, dtype=pl.Int32))
+    candidate_items = pl.concat_list(["eval_interactions", "negative_samples"])
+    return data.select(
+        "user_id", "eval_seqs", candidate_items.alias("candidate_items")
+    ).collect()
+
+
+def write_parquet_data(data_dir: Path, data: pl.DataFrame, prefix: str):
+    write_dir = data_dir.joinpath("parquet_bert4rec")
+    Path.mkdir(write_dir, exist_ok=True)
+    file_unit = math.ceil(len(data) / FILE_NUM)
+    for i, offset in enumerate(range(0, len(data), file_unit), start=1):
+        print(f"writing {prefix} part_{i}...")
+        start = time.perf_counter()
+        part = data.slice(offset, file_unit)
+        if prefix == "train":
+            print("shuffling...")
+            part = part.sample(fraction=1.0, shuffle=True, seed=42)
+
+        part.write_parquet(write_dir.joinpath(f"{prefix}_part_{i}.parquet"))
+        print(f"{prefix} part_{i} finished in {(time.perf_counter() - start):.2f}s")
+
+
+def main():
+    config = read_configs()
+    np_rng = np.random.default_rng(config.seed)
+
+    start_time = time.perf_counter()
+    interaction_data = read_original_data(config.data_dir)
+    print(f"data reading finished in {(time.perf_counter() - start_time):.2f}s")
+    print(f"data size: {interaction_data['user_id'].len():,}")
+
+    data, n_users, n_items = map_ids(interaction_data)
+    items, item_probs = get_item_popularity(data)
+    print(f"n_users: {n_users:,}, n_items: {n_items:,}")
+    with open(config.data_dir / "size_map_bert4rec.json", "w") as f:
+        json.dump({"n_users": n_users, "n_items": n_items}, f, indent=4)
+
+    start_time = time.perf_counter()
+    data = split_data(data)
+    print(f"data split finished in {(time.perf_counter() - start_time):.2f}s")
+    # print(data.row(by_predicate=pl.col("user_id") == 1))
+
+    start_time = time.perf_counter()
+    masked_train_data = gen_train_mask_data(data, n_items, config.mask_prob, np_rng)
+    print(f"total masked ratio: {get_masked_ratio(masked_train_data):.4f}")
+    masked_seq_train_data = gen_sliding_seq(
+        masked_train_data, config.max_len, config.sliding_step
+    )
+    print(f"train seq data finished in {(time.perf_counter() - start_time):.2f}s")
+    print(f"train seq data size: {len(masked_seq_train_data):,}\n")
+    # print(masked_seq_train_data.filter(pl.col("user_id") == 2).glimpse())
+    write_parquet_data(config.data_dir, masked_seq_train_data, prefix="train")
+
+    start_time = time.perf_counter()
+    eval_data = gen_eval_data(data, config.max_len, np_rng, items, item_probs)
+    print(f"\neval seq data finished in {(time.perf_counter() - start_time):.2f}s")
+    print(f"eval seq data size: {len(eval_data):,}")
+    print(
+        f"eval_seq len: {len(eval_data.row(0)[1])}, candidate num: {len(eval_data.row(0)[2])}"
+    )
+    write_parquet_data(config.data_dir, eval_data, prefix="eval")
 
 
 if __name__ == "__main__":
-    import utils
-    config = utils.read_configs()
-    pp = Preprocessor(config)
-    df = pp.get_processed_dataframes()
-    print(df["train"].columns)
-    print(df["train"]["seqs"])
-    print()
-    print(df["val"].columns)
-    print(df["val"])
+    main()
